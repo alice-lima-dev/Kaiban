f596cf73ede57b8a6468f571efcbf306d0ce9632
alice-lima-dev <125032253+alice-lima-dev@users.noreply.github.com>
2023-08-11 09:02:23 -0300

    Add files via upload
    Commit inicial após começar recriar o repositório

---
+++ js.js
@@ -0,0 +1,15 @@
+function login() {
+    var email = document.getElementById("email")
+    var senha = document.getElementById("senha")
+
+    if (email.value == "teste@gmail.com") {
+        alert("Bem vinda geovanna")
+    } else {
+        alert("Você não é a Geovanna")
+    }
+    if (senha.value == "12345") {
+        alert("Tudo pronto para você geh")
+    } else {
+        alert("Some katileia")
+    }
+}
